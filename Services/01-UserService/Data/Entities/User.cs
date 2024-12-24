4488dca3c80cd20229f577e738192491779305d7
Oren Kats <orenkats95@gmail.com>
2024-12-24 09:02:19 +0200

    Events re structure

--- Services/01-UserService/Data/Entities/User.cs
+++ Services/01-UserService/Data/Entities/User.cs
@@ -5,8 +5,9 @@ namespace UserService.Data.Entities
     public class User
     {
         public Guid Id { get; set; }
-        public string? Name { get; set; }
-        public string? PasswordHash { get; set; }
+        public string Name { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public string PasswordHash { get; set; } = null!;
         public decimal AccountBalance { get; set; }
         public DateTime CreatedAt { get; set; }
     }
